934435d428d8c81e187bac00f27bc68890b92c7a
Wellington <28570773+wellingtonollv@users.noreply.github.com>
2023-06-06 01:50:46 -0300

    :recycle: refactor: buffer outside try/catch and log add buffer

--- ws.js
+++ ws.js
@@ -5,8 +5,8 @@ const wss = new webSocket.Server({ port: 8080 });
 wss.on("connection", (ws) => {
   console.log("[LOG] client connected.");
   ws.on("message", (message) => {
+    const bufferString = message.toString("utf8");
     try {
-      const bufferString = message.toString("utf8");
       const jsonObject = JSON.parse(bufferString);
       if (jsonObject.method === "HEART_RATE") {
         wss.clients.forEach((client) => {
@@ -16,8 +16,7 @@ wss.on("connection", (ws) => {
         });
       }
     } catch (error) {
-      console.log("[ERROR] INVALID MESSAGE", error?.message);
-      
+      console.log("[ERROR] INVALID MESSAGE TYPE", error?.message, "message:", bufferString);
     }
   });
 
