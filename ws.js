78838c591fa2625a4f7fb29056c80f92fc4d473f
Wellington <28570773+wellingtonollv@users.noreply.github.com>
2023-06-06 01:26:39 -0300

    simple ws server

---
+++ ws.js
@@ -0,0 +1,32 @@
+const webSocket = require("ws");
+
+const wss = new webSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("[LOG] A client connected.");
+  ws.on("message", (message) => {
+    try {
+      const bufferString = message.toString("utf8");
+      const jsonObject = JSON.parse(bufferString);
+      if (jsonObject.method === "HEART_RATE") {
+        wss.clients.forEach((client) => {
+          if (client.readyState === webSocket.OPEN) {
+            client.send(message);
+          }
+        });
+      }
+    } catch (error) {
+      console.log("[ERROR] PARSING MESSAGE", error?.message);
+      wss.clients.forEach((client) => {
+        if (client.readyState === webSocket.OPEN) {
+          client.send(message);
+        }
+      });
+    }
+  });
+
+  // Handle disconnection
+  ws.on("close", () => {
+    console.log("A client disconnected.");
+  });
+});
